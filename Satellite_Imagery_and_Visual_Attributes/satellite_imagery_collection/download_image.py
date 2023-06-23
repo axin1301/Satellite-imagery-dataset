d9d921e63112dd900d625829e0cfcaa8722c84d2
xyx <82051300+axin1301@users.noreply.github.com>
2023-06-23 23:30:44 +0300

    Update download_image.py

--- Satellite_Imagery_and_Visual_Attributes/satellite_imagery_collection/download_image.py
+++ Satellite_Imagery_and_Visual_Attributes/satellite_imagery_collection/download_image.py
@@ -23,85 +23,94 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 import glob
 
-def subtask(order,Npara,Nlp,full_pd,token):
+def subtask(order,Npara,Nlp,csv_file_list,token):
     order = int(order)
     if order == Npara - 1:
-        lines = list(range(order * Nlp, len(f_list)))
-        #print(lines)
+        lines = list(range(order * Nlp, len(csv_file_list)))
+        print(lines)
     else:
         #print(order)
         lines = list(range(order * Nlp, (order + 1) * Nlp))
-        #print(lines)
+        print(lines)
     print(str(order) + '  subtask_working')
 
-    base_url = 'https://wayback.maptiles.arcgis.com/arcgis/rest/services/World_Imagery/WMTS/1.0.0/default028mm/MapServer/tile/14765/'
+    base_url = 'https://wayback.maptiles.arcgis.com/arcgis/rest/services/World_Imagery/WMTS/1.0.0/default028mm/MapServer/tile/48376/'  #2021 #########important number
     
-    new_lines = full_pd.iloc[lines,:]
-    new_lines.reset_index(inplace=True)
-    print(len(new_lines))
-    cities = list(set(list(new_lines['city'])))
-    print(len(cities))
-    for city in cities:
+    new_lines = [csv_file_list[x] for x in lines]
+    print(new_lines)
+    for csv_file in new_lines:
+        city = csv_file.split('/')[-1].split('.')[0]
+        #if city[0] not in ['F','G','H','I','J','L']:
+        #    continue
+
         Dir = 'zl19_images/'+str(year)+'/'+city
         if not os.path.exists(Dir):
             os.makedirs(Dir)
+        print(csv_file)
+        print(city)
 
-    msk = np.zeros((256*2, 256*2,3))
-    for i in range(len(new_lines)):
-        y_tile = int(new_lines.at[i,'y_tile'])
-        x_tile = int(new_lines.at[i,'x_tile'])
-            #print(y_tile, x_tile)
-            
+        csv_file_data = pd.read_csv(csv_file)
+        for i in range(len(csv_file_data)):
+
+            msk = np.zeros((256*2, 256*2,3))
+    
+            y_tile = int(csv_file_data.at[i,'y_tile'])
+            x_tile = int(csv_file_data.at[i,'x_tile'])
+            print(y_tile, x_tile)
+           
+            if os.path.exists(Dir+'/'+str(y_tile)+'_'+str(x_tile)+'.png'):
+                continue
+
             #if str(y_tile)+'_'+str(x_tile) in download_list:
             #    continue;
-        filename1 = Dir + '/' + str(y_tile*2) + '_' + str(x_tile*2) + '.png'
-        filename2 = Dir + '/' + str(y_tile*2+1) + '_' + str(x_tile*2) + '.png'
-        filename3 = Dir + '/' + str(y_tile*2) + '_' + str(x_tile*2+1) + '.png'
-        filename4 = Dir + '/' + str(y_tile*2+1) + '_' + str(x_tile*2+1) + '.png'
+            filename1 = Dir + '/' + str(y_tile*2) + '_' + str(x_tile*2) + '.png'
+            filename2 = Dir + '/' + str(y_tile*2+1) + '_' + str(x_tile*2) + '.png'
+            filename3 = Dir + '/' + str(y_tile*2) + '_' + str(x_tile*2+1) + '.png'
+            filename4 = Dir + '/' + str(y_tile*2+1) + '_' + str(x_tile*2+1) + '.png'
 
-        url1 = base_url + str(19) + '/' + str(y_tile*2) + '/' + str(x_tile*2)   +'?token='+token
-        url2 = base_url + str(19) + '/' + str(y_tile*2+1) + '/' + str(x_tile*2)   +'?token='+token
-        url3 = base_url + str(19) + '/' + str(y_tile*2) + '/' + str(x_tile*2+1)   +'?token='+token
-        url4 = base_url + str(19) + '/' + str(y_tile*2+1) + '/' + str(x_tile*2+1)   +'?token='+token
+            url1 = base_url + str(19) + '/' + str(y_tile*2) + '/' + str(x_tile*2)   +'?token='+token
+            url2 = base_url + str(19) + '/' + str(y_tile*2+1) + '/' + str(x_tile*2)   +'?token='+token
+            url3 = base_url + str(19) + '/' + str(y_tile*2) + '/' + str(x_tile*2+1)   +'?token='+token
+            url4 = base_url + str(19) + '/' + str(y_tile*2+1) + '/' + str(x_tile*2+1)   +'?token='+token
             #print('url',url4)
 
-        try:
-            urllib.request.urlretrieve(url1,filename1)
-            urllib.request.urlretrieve(url2,filename2)
-            urllib.request.urlretrieve(url3,filename3)
-            urllib.request.urlretrieve(url4,filename4)
-        except:
-            sleep(1)
-            urllib.request.urlretrieve(url1,filename1)
-            urllib.request.urlretrieve(url2,filename2)
-            urllib.request.urlretrieve(url3,filename3)
-            urllib.request.urlretrieve(url4,filename4)
-        im1 = cv.imread(filename1)
-        im2 = cv.imread(filename2)
-        im3 = cv.imread(filename3)
-        im4 = cv.imread(filename4)
-        msk[:256,:256,:] = im1
-        msk[256:,:256,:] = im2
-        msk[:256,256:,:] = im3
-        msk[256:,256:,:] = im4
-        cv.imwrite(Dir+'/'+str(y_tile)+'_'+str(x_tile)+'.png', msk)
+            try:
+                urllib.request.urlretrieve(url1,filename1)
+                urllib.request.urlretrieve(url2,filename2)
+                urllib.request.urlretrieve(url3,filename3)
+                urllib.request.urlretrieve(url4,filename4)
+            except:
+                sleep(2)
+                urllib.request.urlretrieve(url1,filename1)
+                urllib.request.urlretrieve(url2,filename2)
+                urllib.request.urlretrieve(url3,filename3)
+                urllib.request.urlretrieve(url4,filename4)
+            im1 = cv.imread(filename1)
+            im2 = cv.imread(filename2)
+            im3 = cv.imread(filename3)
+            im4 = cv.imread(filename4)
+            msk[:256,:256,:] = im1
+            msk[256:,:256,:] = im2
+            msk[:256,256:,:] = im3
+            msk[256:,256:,:] = im4
+            cv.imwrite(Dir+'/'+str(y_tile)+'_'+str(x_tile)+'.png', msk)
             #print('good')
-        os.remove(filename1)
+            os.remove(filename1)
             #print('remove_op done1')
-        os.remove(filename2)
+            os.remove(filename2)
             #print('remove_op done2')
-        os.remove(filename3)
+            os.remove(filename3)
             #print('remove_op done3')
-        os.remove(filename4)
+            os.remove(filename4)
             #print('remove_op done4')
 
 
-def main():
+def main(csv_file_list):
     p = Pool()
     # print(Npara)
     for i in range(Npara):
         # print(i)
-        p.apply_async(func=subtask, args=(str(i),Npara,Nlp,full_pd,token,))
+        p.apply_async(func=subtask, args=(str(i),Npara,Nlp,csv_file_list,token,))
     p.close()
     p.join()
     print('All subprocesses done.')
@@ -110,61 +119,26 @@ def main():
 
 if __name__ == '__main__':
     global Npara
-    Npara = 100
+    Npara = 4
     global year
-    year = 2017
+    year = 2021     #########important
     time_start = time.time()
-    # token = "AAPKa7a4264aebcd4ecc96154431a9b5689e0koHP6jXg1hMciQYnJOT-F2JWPFQPKUW-gfYPNXj3iMj3yQJBvuso1rQCyIJ6USM"
     global token
-    token = 'AAPK868a38acc94c49c6b5fe93ec72d782848w26DEckXxsIoRYE1oI_rnk57Q3YYwSCh9xYvKNT5cmcS2N5v5RHyL-TZrX1jVDA'
-    undone_file = pd.read_csv('undone_file/statis_2017_undone.csv')
-    city_list = list(undone_file[undone_file['LikeOld']==1]['city'])
-    city = city_list[0]
-    global full_pd
-    full_pd = pd.read_csv('../../tilefile_scd/'+city+'.csv')
-    full_pd['city'] = city
-    for city in city_list[1:]:
-        if city == 'Anchorage municipality':
-            continue
-        df = pd.read_csv('../../tilefile_scd/'+city+'.csv')
-        df['city'] = city
-        full_pd = pd.concat([full_pd, df])
+    token = "AAPKa7a4264aebcd4ecc96154431a9b5689e0koHP6jXg1hMciQYnJOT-F2JWPFQPKUW-gfYPNXj3iMj3yQJBvuso1rQCyIJ6USM"
+    #global token
+    #token = 'AAPK868a38acc94c49c6b5fe93ec72d782848w26DEckXxsIoRYE1oI_rnk57Q3YYwSCh9xYvKNT5cmcS2N5v5RHyL-TZrX1jVDA'
+    
+    csv_file_list = glob.glob('../../../../../Cities_and_CBGs_Boundaries_and_Statistics/tilefile_scd/*.csv')#[:20]
+    print(len(csv_file_list))
+    
+    # city_tmp_list = ['Henderson city','Houston city','Indianapolis city (balance)','Los Angeles city']
+    # csv_file_list = ['tilefile_scd/'+x+'.csv' for x in city_tmp_list]
 
-    print(len(full_pd))
-    ######### new_down
-    downloaded_file = []
-    for city in city_list:
-        downloaded_file = downloaded_file +  glob.glob('zl19_images/'+str(year)+'/'+city+'/*.png')
-    print(len(downloaded_file))
-    ######### old_down
-    """
-    undone_file.fillna(0, inplace=True)
-    city_list2 = list(undone_file[undone_file['LikeOld']==1]['dir_name'])
-    for city in city_list2:
-        #print(city)
-        if not os.path.exists('../zl19_images/'+str(year)+'/'+str(city)):
-            continue 
-        downloaded_file = downloaded_file +  glob.glob('../zl19_images/'+str(year)+'/'+city+'/*.png')
-    print(len(downloaded_file))
-    """
-    downloaded_list = [x.split('/')[-1].split('.')[0] for x in downloaded_file]
-    print(downloaded_list[:10])
-    dd_y = [int(x.split('_')[0]) for x in downloaded_list]
-    dd_x = [int(x.split('_')[1]) for x in downloaded_list]
-    dd_dict = pd.DataFrame({'y_tile':dd_y, 'x_tile':dd_x})
-    print(len(dd_dict))
-    down_oldtime = pd.read_csv('down_oldtime/'+str(year)+'.csv')
-    full_pd = pd.concat([full_pd, dd_dict, down_oldtime])
-    full_pd.drop_duplicates(subset = ['y_tile','x_tile'], keep = False, inplace = True)
-    full_pd.reset_index(drop=True)
-    #print(full_pd.head(5))
-    #print(full_pd.tail(5))
-    print(len(full_pd))
 
     global Nlp
-    Nlp = int(len(full_pd) / Npara)
+    Nlp = int(len(csv_file_list) / Npara)
 
-    main()
+    main(csv_file_list)
 
     time_end = time.time()
     print('cost_time', time_end - time_start, 's')
@@ -181,6 +155,8 @@ if __name__ == '__main__':
 #     'https://wayback.maptiles.arcgis.com/arcgis/rest/services/World_Imagery/WMTS/1.0.0/default028mm/MapServer/tile/645/',      #2019.6.26
 #     'https://wayback.maptiles.arcgis.com/arcgis/rest/services/World_Imagery/WMTS/1.0.0/default028mm/MapServer/tile/11135/']    #2020.6.10
 #     'https://wayback.maptiles.arcgis.com/arcgis/rest/services/World_Imagery/WMTS/1.0.0/default028mm/MapServer/tile/48376/'     #2021.6.9
+#     'https://wayback.maptiles.arcgis.com/arcgis/rest/services/World_Imagery/WMTS/1.0.0/default028mm/MapServer/tile/44710/'  #2022
+#     'https://wayback.maptiles.arcgis.com/arcgis/rest/services/World_Imagery/WMTS/1.0.0/default028mm/MapServer/tile/46399/'  #2023
 
 
 # ]
