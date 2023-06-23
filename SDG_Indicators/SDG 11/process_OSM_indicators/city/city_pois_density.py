c27d8a8d6987f9f718b305d13f7fe779aef70cdb
xyx <82051300+axin1301@users.noreply.github.com>
2023-06-23 22:51:34 +0300

    Update city_pois_density.py

--- SDG_Indicators/SDG 11/process_OSM_indicators/city/city_pois_density.py
+++ SDG_Indicators/SDG 11/process_OSM_indicators/city/city_pois_density.py
@@ -3,26 +3,12 @@ from pathlib import Path
 import glob
 import geopandas as gpd
 
-"""
-OUTPUT_PATH = Path('./temp_output/walkability/daily_living_score')
-OUTPUT_PATH.mkdir(exist_ok=True, parents=True)
 
-poi = pd.read_csv('./temp_output/poi/poi.csv',dtype='str')
-
-selected_naics = ['4451','44551','445110','44512','445120','71219','481','482','483','485']
-poi['NAICSCode_3'] = poi['NAICSCode'].map(lambda x: x[:3])
-
-poi = poi[poi['NAICSCode'].isin(selected_naics) | poi['NAICSCode_3'].isin(selected_naics)].drop(['NAICSCode_3'],axis=1)
-poi['Count'] = pd.Series([1 for _ in range(poi.shape[0])])
-poi = poi[['CityName','CityCode','Count']]
-poi = poi.groupby(['CityName','CityCode']).sum().reset_index()
-"""
-
-area_lut = pd.read_csv('./Area_Lut_city.csv')
+area_lut = pd.read_csv('../../../../../Cities_and_CBGs_Boundaries_and_Statistics/Area_Lut_city.csv')
 # STATEFP,PLACEFP,GEOID,NAME_SHP,NAME_process,ALAND,AWATER,AREA_CAL
 
 #input_files = sorted([i for i in Path('./temp_output/osm_filter/pois/city_level').glob('*.geojson')])
-input_files = glob.glob('../out_geojson_pois/*.geojson')
+input_files = glob.glob('../../../extract_OSM_indicators/out_geojson2023/out_geojson_pois/*.geojson')
 
 area_list = []
 pois_num_list = []
@@ -34,8 +20,6 @@ year_list = []
 for f in input_files:
     print(f)
     city_name = f.split('/')[-1].split('.')[0].split('_')[0]
-    if city_name in ['Anchorage municipality','Urban Honolulu CDP']:
-        continue
     df = gpd.read_file(f, driver='GeoJSON')
     pois_num = df.shape[0]
     #city_name = f.split('/')[-1].split('.')[0].split('_')[0]
@@ -50,7 +34,4 @@ for f in input_files:
 
 df = pd.DataFrame({'CityName':city_name_list, 'Year':year_list, 'PoiNum':pois_num_list, 'Area':area_list, 'PoiDensity':pois_density_list})
 df = df.sort_values(by=['CityName','Year'],ascending=True)
-df.to_csv('daily_living_score_city.csv',index=False)
-
-
-    #poi['DailyLivingScore'] = poi['Count'] / poi['Area']
+df.to_csv('daily_living_score_city2023.csv',index=False)
