baa0d6ba32d08fa4824ed1bb1ff4733255a4cd18
xyx <82051300+axin1301@users.noreply.github.com>
2023-06-23 20:37:15 +0300

    Update population_statistics_cbg.py

--- Cities_and_CBGs_Boundaries_and_Statistics/population_statistics_cbg.py
+++ Cities_and_CBGs_Boundaries_and_Statistics/population_statistics_cbg.py
@@ -12,7 +12,7 @@ year_list = []
 WP_population = []
 
 for year in range(2014,2021):
-    f_list = glob.glob('cropped_popu_cbg/'+str(year)+'/*/*.tif')
+    f_list = glob.glob('../SDG_Indicators/SDG 10/WorldPop/cropped_popu_cbg/'+str(year)+'/*/*.tif')
     for f in f_list:
         city_name = f.split('/')[-1].split('.')[0]
         pop_file = gdal.Open(f)
@@ -24,61 +24,3 @@ for year in range(2014,2021):
 
 pd_dict = pd.DataFrame({'Name_process':city_list,'Year':year_list, 'WP_population':WP_population})
 pd_dict.to_csv('population_statistics_cbg_level.csv', index=False)
-
-
-
-"""
-file_list = glob.glob('NTL_GINI/pop_clip_old/'+str(2016)+'/*.tif')
-city_list = [x.split('/')[-1].split('.')[0] for x in file_list]
-pop_array = np.zeros((len(city_list),5))
-
-for city in city_list:
-    row = int(city_list.index(city))
-    for year in range(2016,2021):
-        col = year - 2016
-        pop_file = gdal.Open('NTL_GINI/pop_clip_old/'+str(year)+'/'+city+'.tif')
-        pop_data = pop_file.ReadAsArray()
-        pop_data[pop_data<0] = 0
-        pop_array[row,col] = np.sum(pop_data)
-
-col_names = [2016,2017,2018,2019,2020]
-data_df = pd.DataFrame(pop_array, columns = col_names)
-pd_dict = pd.DataFrame({'city':city_list})
-pd_final = pd.concat([pd_dict, data_df], axis = 1)
-pd_final.to_csv('population_statistics_old.csv', index=False)
-"""        
-
-
-"""
-for year in [2016,2017,2018]:
-    country_tif=gdal.Open('pop_USA_mainland_'+str(year)+'.tif')
-    country = country_tif.ReadAsArray()
-    print('country.shape', country.shape)
-
-    tmpGeoTransform = country_tif.GetGeoTransform()
-
-    csv_file_list = glob.glob('tilefile_multiple_zl_lnglat/*/*.csv')
-    for f in csv_file_list:
-        d = pd.read_csv(f)
-        print(str(csv_file_list.index(f))+str('/')+str(len(csv_file_list)))
-        zl = int(f.split('/')[1])
-
-        if not os.path.exists('tile_popu/tilefile_multiple_zl_popu_'+str(year)+'/'+str(zl)):
-            os.makedirs('tile_popu/tilefile_multiple_zl_popu_'+str(year)+'/'+str(zl))
-    
-        d['pop'] = 0.0
-
-        for i in range(len(d)):
-            y_init = int(math.floor((d.at[i,'top_left_lat'] -tmpGeoTransform[3])/tmpGeoTransform[5]))
-            x_init = int(math.floor((d.at[i,'top_left_lng'] -tmpGeoTransform[0])/tmpGeoTransform[1]))
-            x_end = int(math.ceil((d.at[i,'bottom_right_lng']-tmpGeoTransform[0])/tmpGeoTransform[1]))
-            y_end = int(math.ceil((d.at[i,'bottom_right_lat']-tmpGeoTransform[3])/tmpGeoTransform[5]))
-
-        #print(x_init,y_init,x_end,y_end)
-            popu_tmp = np.array(country_tif.ReadAsArray(x_init,y_init,x_end-x_init,y_end-y_init))
-            popu_tmp[popu_tmp<0] = 0
-            d.at[i,'pop'] = np.sum(popu_tmp)
-
-        d.to_csv('tile_popu/tilefile_multiple_zl_popu_'+str(year)+'/'+str(zl)+'/'+f.split('/')[-1], index=False)
-
-"""
