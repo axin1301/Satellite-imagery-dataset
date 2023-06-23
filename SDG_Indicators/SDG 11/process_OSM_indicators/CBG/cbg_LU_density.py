c2d9f67d0bd04cc8c09656f4912b37f002099bc4
xyx <82051300+axin1301@users.noreply.github.com>
2023-06-23 23:07:37 +0300

    Update cbg_LU_density.py

--- SDG_Indicators/SDG 11/process_OSM_indicators/CBG/cbg_LU_density.py
+++ SDG_Indicators/SDG 11/process_OSM_indicators/CBG/cbg_LU_density.py
@@ -7,11 +7,10 @@ from pathlib import Path
 import glob
 import geopandas as gpd
 
-#typ = 'driving'
-#area_lut = pd.read_csv('Area_Lut_cbg.csv')
-#area_lut = pd.read_csv('Area_Lut_cbg19.csv')
-area_lut = pd.read_csv('Area_Lut_cbg20.csv')
-all_city_list = list(pd.read_csv('../../process_post_osm/Area_Lut_city.csv')['NAME_process'])
+
+#area_lut = pd.read_csv('../../../../../Cities_and_CBGs_Boundaries_and_Statistics/Area_Lut_cbg19.csv') #2014 - 2019
+area_lut = pd.read_csv('../../../../../Cities_and_CBGs_Boundaries_and_Statistics/Area_Lut_cbg20.csv') # 2020 - 2023
+all_city_list = list(pd.read_csv('../../../../../Cities_and_CBGs_Boundaries_and_Statistics/Area_Lut_city.csv')['NAME_process'])
 #landuse_labels = ['grass', 'commercial', 'recreation_ground', 'churchyard', 'industrial', 'pond', 'reservoir', 'construction', 'basin', 'residential', 'railway', 'farmland', 'cemetery', 'quarry', 'retail', 'forest']
 landuse_labels = ['commercial', 'industrial', 'construction', 'residential']
 LABEL_NAMES = np.asarray(landuse_labels)
@@ -29,16 +28,12 @@ for typ in ['LU']:
     #for year in range(14,20):
     for year in range(20,21):
         for c in all_city_list:
-            #if c == 'Anchorage municipality' or c == 'Urban Honolulu CDP' or c == 'Santa Clarita city' or c == 'San Bernardino city':
-            #    continue
-            if c not in ['Santa Clarita city','San Bernardino city']:
-                continue
             print(year, all_city_list.index(c))
             #df = pd.read_csv(typ+'/'+c+'_cbg_'+typ+'_'+str(year)+'.csv')
             df = pd.read_csv('sjoin_2city/'+c+'_cbg_'+typ+'_'+str(year)+'.csv')
             #print(df.columns)
-            #shapefile = gpd.read_file('../../out_geojson_'+typ+'/'+c+'_'+typ+'_'+str(year)+'.geojson', driver = 'geojson')
-            shapefile = gpd.read_file('../../out_geojson_2city/'+c+'_'+typ+'_'+str(year)+'.geojson', driver = 'geojson')
+            #shapefile = gpd.read_file('../../../extract_OSM_indicators/CBG/out_geojson_'+typ+'/'+c+'_'+typ+'_'+str(year)+'.geojson', driver = 'geojson')
+            
             #print(shapefile1.columns)
             cbg_list = list(set(list(df['CensusBlockGroup'])))
             for cbg in cbg_list:
@@ -55,12 +50,7 @@ for typ in ['LU']:
                 df_final = shapefile_id_list.groupby(['landuse']).agg({'area':'sum'})
                 #print(df_final)
 
-                #for lu in landuse_labels:
-                #    if lu in df_final.index:
-                #        print(df_final.loc[lu,'area'])
-                #    else:
-                #        print(0)
-				
+
                 area_lut_for_city = area_lut.loc[area_lut.CensusBlockGroup == cbg]
                 area_list.append(area_lut_for_city['area'].iloc[0])
                 tmp_seg_results = {}
@@ -84,6 +74,5 @@ for typ in ['LU']:
 
     xxx = pd.DataFrame({'CityName':city_name_list_all,'CensusBlockGroup':cbg_list_all,'Year':year_list,'Area':area_list})
     df_seg_concat = xxx.merge(segmentation_df.reset_index(),on = ['CensusBlockGroup','Year'])
-    #df_seg_concat.to_csv('LU_density_cbg_new_1419.csv',index=False)
-    #df_seg_concat.to_csv('LU_density_cbg_new_1419_2city.csv',index=False)
-    df_seg_concat.to_csv('LU_density_cbg_new_20_2city.csv',index=False)
+    #df_seg_concat.to_csv('LU_density_cbg_new_1419.csv',index=False) # 2014-2019
+    df_seg_concat.to_csv('LU_density_cbg_new_20.csv',index=False) #2020
