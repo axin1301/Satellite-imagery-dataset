24a36034bcab188c7d65b7a95d466ee5d464965a
xyx <82051300+axin1301@users.noreply.github.com>
2023-06-23 20:04:33 +0300

    Update process_cbg_area.py

--- Cities_and_CBGs_Boundaries_and_Statistics/process_cbg_area.py
+++ Cities_and_CBGs_Boundaries_and_Statistics/process_cbg_area.py
@@ -5,8 +5,8 @@ import glob
 import geopandas as gpd
 
 #shp_list = glob.glob('../../cbg_in_city_new/*.geojson')
-#shp_list = glob.glob('../../../ACS/cbg_in_city_new_2/*.geojson')
-shp_list = glob.glob('../../../ACS/cbg_in_city_new_2_2020/*.geojson')
+#shp_list = glob.glob('../../../ACS/cbg_in_city_new/*.geojson')#2014to2019
+shp_list = glob.glob('../../../ACS/cbg_in_city_new_2020/*.geojson') #2020to2023
 
 df_final = pd.DataFrame()
 cnt = 0
@@ -24,24 +24,8 @@ for shp in shp_list:
     df_final = pd.concat([df_final,d2])
     cnt+=len(d)
 print(df_final)
+#df_final[['StateFIPS', 'CountyFIPS', 'TractCode', 'BlockGroup','CensusBlockGroup', 'State', 'County', 'area']].to_csv('Area_Lut_cbg19.csv', index = False)
 df_final[['StateFIPS', 'CountyFIPS', 'TractCode', 'BlockGroup','CensusBlockGroup', 'State', 'County', 'area']].to_csv('Area_Lut_cbg20.csv', index = False)
 print(len(df_final))
 print(cnt)
 
-"""
-OUTPUT_PATH = Path('./output/environmental_determinants/basic_statistics/area')
-OUTPUT_PATH.mkdir(exist_ok=True, parents=True)
-
-city_lut = pd.read_csv('./city_defination_and_LUTs/city_look_up_table.csv')
-city_lut = city_lut[['CityName','CityCode']]
-city_lut = city_lut.drop_duplicates()
-
-area = pd.read_csv('./city_defination_and_LUTs/Major_Towns_and_Cities_(December_2015)_Boundaries_V2.csv')[['TCITY15CD','Shape__Area']]
-area = city_lut.merge(area, left_on = 'CityCode', right_on = 'TCITY15CD',how='left')
-area['Area'] = area['Shape__Area']
-area = area[['CityName','CityCode','Area']]
-area['Area'] /= 1000000
-area = area.sort_values(['CityCode'], ascending=True)
-
-area.to_csv(OUTPUT_PATH.joinpath('area_city.csv'),index=False)
-"""
