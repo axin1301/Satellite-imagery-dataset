6a2d6b5ec23bdfd9c721170f1606550fe5da17c5
xyx <82051300+axin1301@users.noreply.github.com>
2023-06-23 22:41:24 +0300

    Update city_LU_density.py

--- SDG_Indicators/SDG 11/process_OSM_indicators/city/city_LU_density.py
+++ SDG_Indicators/SDG 11/process_OSM_indicators/city/city_LU_density.py
@@ -5,14 +5,12 @@ import matplotlib.pyplot as plt
 import geopandas as gpd
 import glob
 
-#OUTPUT_PATH = Path('./output/environmental_determinants/built_environment/building_density')
-#OUTPUT_PATH.mkdir(exist_ok=True, parents=True)
 
-area_lut = pd.read_csv('../Area_Lut_city.csv')
+area_lut = pd.read_csv('../../../../../Cities_and_CBGs_Boundaries_and_Statistics/Area_Lut_city.csv')
 # STATEFP,PLACEFP,GEOID,NAME_SHP,NAME_process,ALAND,AWATER,AREA_CAL
 
 #input_files = sorted([i for i in Path('./temp_output/osm_filter/building/city_level').glob('*.geojson')])
-input_files = glob.glob('../out_geojson2023/out_geojson_LU/*.geojson')
+input_files = glob.glob('../../../extract_OSM_indicators/out_geojson2023/out_geojson_LU/*.geojson')
 landuse_labels = ['commercial', 'industrial', 'construction', 'residential']
 LABEL_NAMES = np.asarray(landuse_labels)
 
@@ -34,8 +32,6 @@ segmentation_df = pd.DataFrame()
 for f in input_files:
     print(input_files.index(f))
     city_name = f.split('/')[-1].split('.')[0].split('_')[0]
-    #if city_name == 'Anchorage municipality' or city_name == 'Urban Honolulu CDP':
-    #    continue
     #df = gpd.read_file(f, driver='GeoJSON')
     #print(df.columns)
     #city_name = f.split('/')[-1].split('.')[0].split('_')[0]
@@ -65,37 +61,3 @@ for f in input_files:
     #city_name_list_all.append(c)
     segmentation_df = pd.concat([segmentation_df,pd.DataFrame(tmp_seg_results)])
 segmentation_df.to_csv('LU_density_city'+str(20)+str(year)+'.csv', index = False)
-
-
-"""
-    df['geometry'] = df.geometry.to_crs({'proj':'cea'})
-    for i in range(len(df)):
-        d_area = df.at[i,'geometry'].area
-        #print(d_area/1000000)
-        print(area_lut_for_city['AREA_CAL'].iloc[0])
-        df.at[i,'AREA_percentage'] = d_area/1000000/area_lut_for_city['AREA_CAL'].iloc[0]
-
-df = df.groupby(['landuse']).agg({'AREA_percentage':'sum'}).reset_index()
-df.to_csv('tmp_LU/'+city_name+'_'+year+'_LU.csv', index = True)
-"""
-
-"""
-    building_num = df.shape[0]
-    city_name = f.split('/')[-1].split('.')[0].split('_')[0]
-    year = f.split('/')[-1].split('.')[0].split('_')[-1]
-    area_lut_for_city = area_lut.loc[area_lut.NAME_process == city_name]
-
-    #d['geometry'] = d.geometry.to_crs({'proj':'cea'})
-    #d_area = d.at[0,'geometry'].area
-
-    area_list.append(area_lut_for_city['AREA_CAL'].iloc[0])
-    building_num_list.append(building_num)
-    building_density_list.append(building_num/area_lut_for_city['AREA_CAL'].iloc[0])
-    #city_code_list.append(city_code)
-    city_name_list.append(city_name)
-    year_list.append(int(str(20)+str(year)))
-
-    df = pd.DataFrame({'CityName':city_name_list, 'Year':year_list, 'BuildingNum':building_num_list, 'Area':area_list, 'BuildingDensity':building_density_list})
-    df = df.sort_values(by=['CityName','Year'],ascending=True)
-    df.to_csv('building_density_city.csv',index=False)
-"""
